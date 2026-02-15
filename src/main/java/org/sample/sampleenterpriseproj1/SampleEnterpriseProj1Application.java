216fe61a038c0ece62e6bcb50986da592d13b669
veera-yerramsetty-usa <veera.y2025@gmail.com>
2026-02-15 09:01:13 -0800

    Project initiated

---
+++ src/main/java/org/sample/sampleenterpriseproj1/SampleEnterpriseProj1Application.java
@@ -0,0 +1,13 @@
+package org.sample.sampleenterpriseproj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleEnterpriseProj1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleEnterpriseProj1Application.class, args);
+    }
+
+}
